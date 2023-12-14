952971c6a9d49c3e28bffa3a7a1ea567bdb0b8f9
maybejenna <149430949+maybejenna@users.noreply.github.com>
2023-12-13 21:11:39 -0500

    Add files via upload

---
+++ let-and-const-Exercise.js
@@ -0,0 +1,38 @@
+// ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+​
+// ES2015 Global Constants
+const PI = 3.14;
+
+
+// ## **Quiz**
+
+// - What is the difference between ***var*** and ***let***?
+//     the var keyword scopes a variable to that function. 
+//     It hoists to the top of the scope it is defined in. 
+//     You can redeclare it and reasign its value. 
+
+//     the let keyword is a block-scoped variable, so it only exists inside a code block. 
+//     It can be reassigned but not redeclared. 
+
+// - What is the difference between **var** and ***const***?
+//     the var keyword scopes a variable to that function. 
+//     It hoists to the top of the scope it is defined in. 
+//     You can redeclare it and reasign its value. 
+//     It can be mutated
+
+//     the const keyword is a block-scoped variable, so it only exists inside a code block. 
+//     It can NEVER be reassigned but not redeclared. 
+//     It can be mutated
+
+// - What is the difference between **let** and **const**?
+//     let can be reassigned or redeclared.
+//     const CANNOT be reassigned or redeclared.
+
+// - What is hoisting?
+//     Hoisting in JavaScript is a behavior in which variable and function declarations are 
+//     moved to the top of their containing scope during the compilation phase. 
+//     This means that variables and functions are accessible before their actual declaration in the code. 
+
+
